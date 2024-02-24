e8c860ffb4835bc98916a873f647abd550f7dbcc
David <gerson2050@hotmail.com>
2024-02-24 13:13:10 -0500

    first commit

---
+++ src/main/java/com/davidlima/LimaHotelDemo/LimaHotelDemoApplication.java
@@ -0,0 +1,13 @@
+package com.davidlima.LimaHotelDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimaHotelDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimaHotelDemoApplication.class, args);
+	}
+
+}
